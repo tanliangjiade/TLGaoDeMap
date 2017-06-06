ef1055392650758215f1d0739abfae712cc38ad7
tanliangjiade <tanliangjiade@gmail.com>
2017-06-06 11:32:32 +0800

    初始化高德地图项目

---
+++ TLGaodeMap/TLGaodeMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TLGaodeMap
+//
+//  Created by TAN on 2017/6/6.
+//  Copyright © 2017年 zfsoft.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TLGaodeMap/TLGaodeMap/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TLGaodeMap
+//
+//  Created by TAN on 2017/6/6.
+//  Copyright © 2017年 zfsoft.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
